65d2e09223ef1217c56009ca85c088b429313830
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-01-13 09:46:31 -0400

    Configurados template e estilo do componente Jogador.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <main id="app">
-    <h1>Projeto Pokemon</h1>
+    <section id="jogadores">
+      <PlayerComponent />
+    </section>
   </main>
 </template>
 
 <script>
+  import PlayerComponent from './components/PlayerComponent'
 
 export default {
   name: 'App',
   components: {
-    
+    PlayerComponent
   }
 }
 </script>
@@ -53,5 +56,128 @@ export default {
     display: flex;
     flex-direction: column;
     align-items: center;
+  }
+
+  /* Seções */
+
+  #jogadores, #acoes, #logs {
+    box-shadow: 0px 0px 5px #595959;
+    width: 100%;
+  }
+
+  #acoes, #logs, #mensagem-resultado {
+    margin-top: 20px;
+  }
+
+  #jogadores {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  /* Individuais */
+
+  .personagem {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 45%;
+  }
+
+  .personagem h2 {
+    font-size: 0px;
+  }
+
+  .caixa-vida {
+    border: 3px dotted #d9d9d9;
+    width: 100%;
+    height: 40px;
+    margin: 10px 0px;
+  }
+
+  .vida {
+    height: 100%;
+    width: 100%;
+  }
+
+  /* Log da batalha */
+
+  .log-acao {
+    margin: 5px 0px;
+    text-align: center;
+  }
+
+  .monstro {
+    padding: 10px;
+    width: 100%;
+    background-color: #33cc33;
+    color: #fff;
+  }
+
+  .jogador {
+    padding: 10px;
+    width: 100%;
+    background-color: #ff3333;
+    color: #fff;
+  }
+
+  /* Botões */
+
+  .btn {
+    padding: 8px 10px;
+    font-weight: bold;
+    margin-right: 20px;
+    border: 3px outset;
+    color: white;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    outline: none;
+  }
+
+  #btn-atacar {
+    background-color: #ff3333;
+    border-color: #ff3333;
+  }
+
+  #btn-especial {
+    background-color: #3399ff;
+    border-color: #3399ff;
+  }
+
+  #btn-curar {
+    background-color: #ffff66;
+    border-color: #ffff66;
+    color: black;
+  }
+
+  #btn-desistir {
+    background-color: #404040;
+    border-color: #404040;
+  }
+
+  #btn-iniciar {
+    background-color: #33cc33;
+    color: #fff;
+    border-color: #33cc33;
+  }
+
+  /* Status da partida */
+
+  .desistiu {
+    background-color: #404040;
+    box-shadow: 0 0 10px #404040;
+    color: #fff;
+  }
+
+  .ganhou {
+    background-color: #33cc33;
+    box-shadow: 0 0 10px #33cc33;
+    color: #fff;
+  }
+
+  .perdeu {
+    background-color: #ff3333;
+    box-shadow: 0 0 10px #ff3333;
+    color: #fff;
   }
 </style>
---
+++ src/components/PlayerComponent.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="personagem espacar">
+    <h2>Jogador<img src="https://img.icons8.com/dotty/80/000000/user.png" /></h2>
+    <div class="caixa-vida">
+      <div class="vida" :style="getVidaJogador"></div>
+    </div>
+    <div>{{ getVidaJogador.width }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        jogador: {
+          vida: 100,
+          ataque: {
+            normal: {
+              minimo: 5,
+              maximo: 10
+            },
+            especial: {
+              minimo: 10,
+              maximo: 20
+            }
+          },
+          cura: {
+            capacidadeMinima: 10,
+            capacidadeMaxima: 20
+          }
+        }
+      }
+    },
+    computed: {
+      getVidaJogador() {
+        return {
+          width: `${this.jogador.vida}%`,
+          backgroundColor: this.getCorBarra(this.jogador.vida)
+        }
+      }
+    },
+    methods: {
+      getCorBarra(vida) {
+        if (vida > 60) {
+          return '#33cc33'
+        } else if (vida > 30) {
+          return '#e6e600'
+        } else {
+          return '#ff471a'
+        }
+      },
+    }
+  }
+</script>
+
+<style>
+  
+</style>
