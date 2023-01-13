7c45159da7bad811d7dbf004a6f7d4374473823c
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-01-13 09:22:53 -0400

    Modificado o template e o estilo da tela inicial.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
-  <div id="app">
-    
-  </div>
+  <main id="app">
+    <h1>Projeto Pokemon</h1>
+  </main>
 </template>
 
 <script>
-
 
 export default {
   name: 'App',
@@ -16,5 +15,43 @@ export default {
 </script>
 
 <style>
+  /* Reset */
+  * {
+    font-family: 'Orbitron', sans-serif;
+    font-weight: normal;
+    font-size: 100%;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
+  h1, h2 {
+    font-weight: bold;
+  }
+
+  h1 {
+    font-size: 2.5rem;
+  }
+
+  h2 {
+    font-size: 2rem;
+  }
+
+  .centralizar {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+  }
+
+  .espacar {
+    padding: 10px;
+  }
+
+  /* ############### CORPO ############### */
+
+  #app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 </style>
