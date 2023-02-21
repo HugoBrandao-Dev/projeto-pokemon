c141d9f0e035442d56fb5b824277ba7d76297e1c
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-02-20 21:23:14 -0400

    Adicionado estilo aos elementos de tipos do pokemon na tela de selecao de pokemon inicial.

--- src/components/AlertComponent.vue
+++ src/components/AlertComponent.vue
@@ -23,8 +23,8 @@
             <figure>
                 <img :src="item.info.pictureId | assemblePokemonImageLink">
                 <ul class="types">
-                  <li v-for="type in item.info.types" :key="type.type" class="type">
-                    <img :src="type.url" :title="type.type" :class="type.type">
+                  <li v-for="type in item.info.types" :key="type.type" class="type" :class="type.type">
+                    <img :src="type.url" :title="type.type">
                   </li>
                 </ul>
                 <figcaption class="pokemon-desc">{{ item.info.specie }} | Exp. {{ item.info.experience }}</figcaption>
@@ -212,5 +212,88 @@
 
   .types {
     display: flex;
+    margin-bottom: 10px;
+  }
+
+  .type {
+    border: ridge 2px #ffffff;
+    width: 32px;
+    height: 32px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .type img {
+    height: 16px;
+  }
+
+  .normal {
+    background-color: #996600;
+  }
+
+  .fighting {
+    background-color: #cc6600;
+  }
+  
+  .grass {
+    background-color: #33cc33;
+  }
+  
+  .poison {
+    background-color: #6600ff;
+  }
+  
+  .ground {
+    background-color: #3e3202;
+  }
+  
+  .rock {
+    background-color: #a6a6a6;
+  }
+  
+  .insect {
+    background-color: #008000;
+  }
+  
+  .ghost {
+    background-color: #262626;
+  }
+  
+  .electric {
+    background-color: #e6e600;
+  }
+  
+  .fire {
+    background-color: #ff0000;
+  }
+  
+  .dragon {
+    background-color: #cc3300;
+  }
+  
+  .water {
+    background-color: #3366ff;
+  }
+  
+  .psychic {
+    background-color: #9900cc;
+  }
+  
+  .ice {
+    background-color: #00ccff;
+  }
+  
+  .wing {
+    background-color: #669999;
+  }
+  
+  .fairy {
+    background-color: #ff0066;
+  }
+
+  .steel {
+    background-color:  #666666;
   }
 </style>
