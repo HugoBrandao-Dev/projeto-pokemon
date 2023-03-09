611aefdad33553347b2e201b9777e82868e762c3
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-03-09 18:37:22 -0400

    Criada secao para o login do usuario e atribuido um estilo para a mesma.

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,15 +1,57 @@
 <template>
   <header id="cabecalho" class="centralizar espacar">
       <h1>Projeto Pokemon</h1>
+      <div class="login">
+        <small v-show="user.hasUser" class="user-name">{{ user.info.login }}</small>
+        <button class="btn btn-login">
+          <img :src="iconUser"/>
+        </button>
+      </div>
   </header>
 </template>
 
 <script>
   export default {
-    
+    data() {
+      return {
+        user: {
+          hasUser: false,
+          info: {
+            login: ''
+          }
+        }
+      }
+    },
+    computed: {
+      iconUser() {
+        if (this.user.hasUser) {
+          return 'https://img.icons8.com/pastel-glyph/32/00cc99/user-male-circle.png'
+        }
+        return 'https://img.icons8.com/pastel-glyph/32/ff3333/user-male-circle.png'
+      }
+    }
   }
 </script>
 
 <style scopted>
-  
+  #cabecalho {
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .login {
+    display: flex;
+  }
+
+  .login .user-name {
+    font-size: 10pt;
+    margin: 20px;
+  }
+
+  .login .btn-login {
+    margin-right: 0;
+    border: none;
+    background-color: #47476b;
+    padding: 0;
+  }
 </style>
