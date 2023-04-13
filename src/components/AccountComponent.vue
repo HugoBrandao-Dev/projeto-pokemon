0d4b0dba6b2d5dbab7a449b50f9935d10f621487
HugoBrandao-Dev <hugobrandao.dev@gmail.com>
2023-04-13 12:34:00 -0400

    Criada nova rota para o component de account.

--- src/components/AccountComponent.vue
+++ src/components/AccountComponent.vue
@@ -1,3 +1,7 @@
-<template></template>
+<template>
+  <div id="account">
+    <h1>Conta</h1>
+  </div>
+</template>
 <script></script>
 <style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../App.vue'
+import Home from '../components/HomeComponent.vue'
+import Account from '../components/AccountComponent.vue'
 
 /*
 import axios from 'axios'
@@ -41,6 +42,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/account',
+    name: 'Account',
+    component: Account
   }
 ]
 
